b5c849f4b9962748492df87078257d4df1b99edd
JoPalermo <129459065+JoPalermo@users.noreply.github.com>
2024-07-06 23:03:11 -0300

    update

--- EDA.py
+++ EDA.py
@@ -1,49 +1,47 @@
-# Importamos las librerías necesarias para el análisis exploratorio de datos.
+# Importamos las librerias necesarias:
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
-# Importamos el dataset de crimenes en Los Angeles.
-crimenes = pd.read_csv('C:/Users/Usuario/Documents/ITBA/Data set - Crimenes en Los Angeles/Crimenes en Los Angeles.csv', usecols=range(2, 28))
+# Cargamos el dataset:
+Trabajos_LinkedIn = pd.read_csv("C:/Users/Usuario/Documents/ITBA/SAT - 81.75/TP - Final - SAT/postings.csv")
 
-# Obtenemos una preview del dataset.
-print("\n--- Preview del dataset ---")
-print(crimenes.head(5))
+# Visualizamos las primeras filas del dataset: 
+print("--- Preview Dataset ---")
+print(Trabajos_LinkedIn.head(5))
 
-# Acomodamos el dataset.
-crimenes.rename(columns={'DATE OCC':'Dia_Ocurrencia', 'TIME OCC':'Tiempo_Ocurrencia', 'AREA':'Cod_Area', 'AREA NAME':'Nom_Area', 'Rpt Dist No':'Num_Distrito', 'Part 1-2':'Parte', 'Crm Cd':'Cod_Crimen', 'Crm Cd Desc':'Desc_Crimen', 'Mocodes':'M_Cod', 'Vict Age':'Edad_Victima', 'Vict Sex':'Sexo_Victima', 'Vict Descent':'Descendencia_Victima', 'Premis Cd':'Cod_Tipo_Ubic', 'Premis Desc':'Desc_Tipo_Ubic', 'Weapon Used Cd':'Cod_Arma', 'Weapon Desc':'Desc_Arma', 'Status':'Cod_Estado', 'Status Desc':'Desc_Estado', 'Crm Cd 1':'Sub_Cod1', 'Crm Cd 2':'Sub_Cod2', 'Crm Cd 3':'Sub_Cod3', 'Crm Cd 4':'Sub_Cod4', 'LOCATION':'Direccion', 'Cross Street':'Interseccion', 'LAT':'Latitud', 'LON':'Longitud'}, inplace=True)
-print("\n--- Filas & Columnas ---")
-print("Cantidad de filas:", crimenes.shape[0])
-print("Cantidad de columnas:", crimenes.shape[1])
+# Visualizamos la cantidad de filas y columnas del dataset:
+print("\n--- Cantidad de filas y columnas ---")
+print(Trabajos_LinkedIn.shape)
 
-print("\n--- Tipos de datos ---")
-columnas = crimenes.columns
-columna_tipo = [crimenes[col].dtype for col in columnas]
-print("Numero Columna\tNombre Columna\t\tTipo de dato")
-for idx, (name, dtype) in enumerate(zip(columnas, columna_tipo)):
-    print(f"{idx + 1}\t\t{name}\t\t{dtype}")
+# Visualizamos los nombres de las columnas:
+print("\n--- Nombres de las columnas ---")
+columnas = Trabajos_LinkedIn.columns
+Nombres_columnas = pd.DataFrame({
+    'Nombre de Columna': columnas
+})
+print(Nombres_columnas)
 
-# Cambio el tipo de datos de mis columnas.
-crimenes['Cod_Area'] = crimenes['Cod_Area'].astype('int', errors='Nada')
-crimenes['Nom_Area'] = crimenes['Nom_Area'].astype('str', errors='Nada')
-crimenes['Num_Distrito'] = crimenes['Num_Distrito'].astype('int', errors='Nada')
-crimenes['Parte'] = crimenes['Parte'].astype('int', errors='Nada')
-crimenes['Cod_Crimen'] = crimenes['Cod_Crimen'].astype('int', errors='Nada')
-crimenes['Desc_Crimen'] = crimenes['Desc_Crimen'].astype('str', errors='Nada')
-crimenes['M_Cod'] = crimenes['M_Cod'].astype('str', errors='Nada')
-crimenes['Edad_Victima'] = crimenes['Edad_Victima'].astype('int', errors='Nada')
-crimenes['Sexo_Victima'] = crimenes['Sexo_Victima'].astype('str', errors='Nada')
-crimenes['Descendencia_Victima'] = crimenes['Descendencia_Victima'].astype('str', errors='Nada')
-crimenes['Cod_Tipo_Ubic'] = crimenes['Cod_Tipo_Ubic'].astype('int', errors='Nada')
-crimenes['Desc_Tipo_Ubic'] = crimenes['Desc_Tipo_Ubic'].astype('str', errors='Nada')
-crimenes['Cod_Arma'] = crimenes['Cod_Arma'].astype('int', errors='Nada')
-crimenes['Desc_Arma'] = crimenes['Desc_Arma'].astype('str', errors='Nada')
-crimenes['Cod_Estado'] = crimenes['Cod_Estado'].astype('int', errors='Nada')
-crimenes['Desc_Estado'] = crimenes['Desc_Estado'].astype('str', errors='Nada')
-crimenes['Sub_Cod1'] = crimenes['Sub_Cod1'].astype('int', errors='Nada')
-crimenes['Sub_Cod2'] = crimenes['Sub_Cod2'].astype('int', errors='Nada')
-crimenes['Sub_Cod3'] = crimenes['Sub_Cod3'].astype('int', errors='Nada')
-crimenes['Sub_Cod4'] = crimenes['Sub_Cod4'].astype('int', errors='Nada')
-crimenes['Direccion'] = crimenes['Direccion'].astype('str', errors='Nada')
-crimenes['Interseccion'] = crimenes['Interseccion'].astype('str', errors='Nada')
-crimenes['Latitud'] = crimenes['Latitud'].astype('float64', errors='Nada')
-crimenes['Longitud'] = crimenes['Longitud'].astype('float64', errors='Nada')
+# Visualizamos la cantidad de valores nulos por columna:
+print("\n--- Valores Nulos x Columna ---")
+Valores_nulos = Trabajos_LinkedIn.isna().sum()
+print(Valores_nulos)
+
+# Corregimos los valores nulos para cada columna:
+print("\n--- Correccion de Valores Nulos ---")
+NaN = {"job_id" : "Unknown", "company_name" : "Unknown", "title" : "Unknown", "description" : "Unknown", "max_salary" : "Unknown", "pay_period" : "Unknown", "location" : "Unknown", "company_id" : "Unknown", "views" : "Unknown", "med_salary" : "Unknown", "min_salary" : "Unknown", "formatted_work_type" : "Unknown", "applies" : "Unknown", "original_listed_time" : "Unknown", "remote_allowed" : "Unknown", "job_posting_url" : "Unknown", "application_url" : "Unknown", "application_type" : "Unknown", "expiry" : "Unknown", "closed_time" : "Unknown", "formatted_experience_level" : "Unknown", "skills_desc" : "Unknown", "listed_time" : "Unknown", "posting_domain" : "Unknown", "sponsored" : "Unknown", "work_type" : "Unknown", "currency" : "Unknown", "compensation_type" : "Unknown",}
+Trabajos_LinkedIn = Trabajos_LinkedIn.fillna(NaN)
+Valores_nulos = Trabajos_LinkedIn.isna().sum()
+print(Valores_nulos)
+
+# Visualizamos la cantidad de valores unicos por columna:
+print("\n--- Valores Unicos x Columna ---")
+Valores_unicos = Trabajos_LinkedIn.nunique()
+print(Valores_unicos)
+
+# Obtenemos los valores descriptivos del dataset:
+print(Trabajos_LinkedIn.describe(include= "all"))
+
+# print(Trabajos_LinkedIn.company_name.value_counts())
 
--- README.md
+++ README.md
@@ -1,8 +1,48 @@
 # TP-Final-SAT
 Alumno --> Palermo Joaquin 60194
 
-Para el desarrollo del EDA se utiliza un Data Set que contiene la información referida a los crimenes ocurridos en la ciudad de Los Angeles en los ultimos años.
+Para llevar a cabo el EDA se utiliza una base de datos con la información pertinente a los trabajos publicados en la plataforma LinkdIn. El objetivo es analizar la oferta de empleo en el último año y el tipo de trabajo y perfiles que demandas las empresas en el último año.
 
------- Seccion 1 - Data Set ------
+Para empezar a entender el Data Set, me fijo que información contiene. Para esto hago una preview de las primeras 5 filas del Data Set. Si bien no me aporta mucha información, ya me puedo empezar a dar una idea de lo que contiene.
+Luego, busco la cantidad de filas y columnas para dimensionar el Data Set.
+- Cantidad de Filas: 123849
+- Cantidad de Columnas: 28
 
-Se comienza importando el Data Set con el nomre "crimenes" para poder relevar la información que contiene y hacer una limpieza de los datos en caso de ser necesario....
+Una vez que sé la cantidad de columnas, quiero ver que contiene cada columna, por lo que extraigo el título de cada una de estas columnas.
+- Nombres de las Columnas:
+    1. job_id
+    2. company_name
+    3. title
+    4. description
+    5. max_salary
+    6. pay_period
+    7. location
+    8. company_id
+    9. views
+    10. med_salary
+    11. min_salary
+    12. formatted_work_type
+    13. applies
+    14. original_listed_time
+    15. remote_allowed
+    16. job_posting_url
+    17. application_url
+    18. application_type
+    19. expiry
+    20. closed_time
+    21. formatted_experience_level
+    22. skills_desc
+    23. listed_time
+    24. posting_domain
+    25. sponsored
+    26. work_type
+    27. currency
+    28. compensation_type
+
+El paso siguiente es ver si la información que contiene cada columna tiene errores o si faltan datos.
+Lo que hago es fijarse mi hay valores faltantes y en caso de que haya, los remplazo por la palabra "Unknown". Podemos ver que en las columnas en las que falta más información es en aquellas relacionadas con el salario ofrecido.
+
+Otra 
+
+
+
